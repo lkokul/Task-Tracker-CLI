30983323309c97c0ca958b453764d351c509a034
koku <mrobe2503@gmail.com>
2025-11-11 13:11:44 +0100

    Add function + class implemented

--- src/main.py
+++ src/main.py
@@ -1,12 +1,44 @@
 import os
-import task_manager as tm
+from task_manager import TaskManager as tm
 
+def clean_terminal():
+    os.system('cls')
+
+clean_terminal()
 while True:
-    cmd = input("task-cli > ").split(" ")
-    if cmd[0].lower() == "help": tm.help()
-    elif cmd[0].lower() == "exit": break
+    try:
+        cmd = input("task-cli > ").split(" ")
+        if cmd[0].lower() == "help": tm.help()
+        elif cmd[0].lower() in ("exit", "e", "close"): break
 
-    elif cmd.[0].lower()
+        elif cmd[0].lower() == "add":
+            description = input("     Description of the task: ")
+            status = input("     Status of the task: ")
 
-    else:
-        tm.help()
+            task = {
+                "description" : description,
+                "status" : status
+            }
+            print(task)
+            
+            obj_task = tm(description=description, status=status)
+            obj_task.add_taks()
+        
+        elif cmd[0].lower() == "delete":
+            task_name = input("     Name of the task to delete: ")
+
+            task = {
+                "task_name" : task_name,
+                "description" : description,
+                "status" : status
+            }
+            print(task)
+            tm.add_taks(task)
+
+        else:
+            tm.help()
+    
+    except Exception as e:
+        print(e)
+
+clean_terminal()
--- src/task_manager.py
+++ src/task_manager.py
@@ -1,4 +1,59 @@
-def help():
-    print('''
-        
-        ''')
+import json
+from datetime import datetime 
+
+class TaskManager:
+    file_name = "task_register.json"
+    count = 0
+    def __init__(self, description, status):
+        self.id = count
+        self.description = description
+        self.status = status
+        self.createdAt = datetime.now()
+        self.updatedAt= None
+
+        TaskManager.count += 1
+
+    def decorator(self, function):
+        def wrapper(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except Exception as e:
+                print(e)
+
+        return wrapper
+
+    @classmethod
+    @decorator
+    def help(cls):
+        """Shows the available methods and how to use them"""
+        print('''
+            - help
+            - exit
+
+            - add
+            - modify
+            - delete
+            ''')
+
+    def add_taks(self):
+        """Writes a task into task_register file"""
+        with open(TaskManager.file_name, "w") as file:
+            task = {
+                "id": self.id,
+                "description": self.description,
+                "status": self.status,
+                "createdAt": self.createdAt,
+                "updatedAt": self.updatedAt
+            }
+            file.write(json.dump(task)) # We use dump or load to write into an archive or to read it
+        print(f'Task added successfully (ID:{task["id"]})')
+
+    def modify_taks(self):
+        """Modifies a task from task_register file"""
+        with open(file_name, "w") as file:
+            file.write(json.dumps(task))
+
+    def delete_taks(self):
+        """Deletes a task from task_register file"""
+        with open(file_name, "w") as file:
+            file.write(json.dumps(task))
